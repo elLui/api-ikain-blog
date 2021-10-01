45f48d599cfdb2be99f5deee4ef305b73ca0371b
elluis <elluis.codes@gmail.com>
2021-10-01 10:18:30 -0500

    Initial commit
    express server up and running on port 5000
    
    also installed dependencies and set up package.json to reboot the server with changes
    on save

---
+++ index.js
@@ -0,0 +1,14 @@
+// create express server named app
+const express = require("express");
+const app = express();
+
+
+//
+
+
+
+
+
+app.listen("5000", () => {
+    console.log("express backend up and running - for real this time - i mean it")
+})
